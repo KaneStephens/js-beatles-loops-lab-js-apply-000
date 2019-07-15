64148fc7ad50fb5975a4edb3aba2c6dabe83590b
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-07-15 12:10:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var aLength = musicians.length
+  var beatlesInfo = []
+  for (let i = 0; i < aLength; i++){
+    beatlesInfo.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatlesInfo
+}
+
+function johnLennonFacts(array){
+  var i = 0
+  while (array[i]){
+    array[i] = `${array[i]}!!!`
+    i++
+  }
+  return array
+}
+
+
+
